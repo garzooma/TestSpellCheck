87c2ab55ba917d591b4471e819dea1e62350ec4f
GREGORY ARZOOMANIAN <greg@arzoosoftware.com>
2020-11-20 16:45:56 -0500

    Test WPF TextBox spellchecking
    Test WPF TextBox spellchecking.  Looking at custom dictionaries.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using System.IO.Packaging;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +26,51 @@ namespace TestSpellCheck
     public MainWindow()
     {
       InitializeComponent();
+    }
+
+    internal static Uri spellUri;
+
+    private void Window_Loaded(object sender, RoutedEventArgs e)
+    {
+      IList list = SpellCheck.GetCustomDictionaries(textBox);
+      string fileName = "customwords.lex";
+      System.IO.StreamReader file = new System.IO.StreamReader(fileName);
+      string line;
+      StringBuilder sb = new StringBuilder();
+      while ((line = file.ReadLine()) != null)
+      {
+        System.Console.WriteLine(line);
+        sb.Append(line);
+        sb.Append("\r\n");
+      }
+
+
+      UTF8Encoding encoding = new UTF8Encoding();
+      var bytes = encoding.GetBytes(sb.ToString());
+
+      // Create in-memory Package to store the byte array
+      MemoryStream stream = new MemoryStream();
+      Package pack = Package.Open(stream, FileMode.Create, FileAccess.ReadWrite);
+      Uri packUri = new Uri("SpellCheckDictionary:");
+      PackageStore.RemovePackage(packUri);
+      PackageStore.AddPackage(packUri, pack);
+      Uri packPartUri = new Uri("/Content", UriKind.Relative);
+      PackagePart packPart = pack.CreatePart(packPartUri, "text/plain");
+      if (packPart != null)
+      {
+        Stream packageStream = packPart.GetStream();
+        packageStream.Write(bytes, 0, bytes.Length);
+        packageStream.Close();
+        spellUri = PackUriHelper.Create(packUri, packPart.Uri);
+      }
+
+      textBox.SpellCheck.CustomDictionaries.Clear();
+      textBox.SpellCheck.CustomDictionaries.Add(spellUri);
+    }
+
+    private void Window_Closed(object sender, EventArgs e)
+    {
+      textBox.SpellCheck.CustomDictionaries.Clear();
     }
   }
 }
